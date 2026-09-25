9dbbb96e8ddc99f9c0f11e2e047afd0b273c4157
laith-rox <alking00222@gmail.com>
2026-09-25 17:41:38 +0300

    Normalize entry insurance test imports

--- test_bridge_emergency_worker.py
+++ test_bridge_emergency_worker.py
@@ -11,7 +11,8 @@ from bridge_emergency_worker import (
     evaluate_profit_guardian,
     protected_profit_floor,
     main_morning_window,
-    main_profit_lock_sl,\n    insured_sl,
+    main_profit_lock_sl,
+    insured_sl,
 )
 
 
